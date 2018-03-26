3e00e42b6e1c274c3400c196924dd7d8409889ac
arnont <arnon010@hotmail.com>
2018-03-26 17:09:23 +0700

    Update miniBus & Polygon Map All

--- app/src/main/java/com/app/arnont/kkminibus/activity/MapAllActivity.java
+++ app/src/main/java/com/app/arnont/kkminibus/activity/MapAllActivity.java
@@ -20,16 +20,34 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
 
 public class MapAllActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private static final int ACCESS_FINE_LOCATION_INTENT_ID = 3;
 
-    String[] nameArray = {"สาย 2 : บ้านโคกฟันโปง – บ้านโคกน้อย","สาย 3 : บขส.3 – บ้านพรหมนิมิตร","สาย 4 : บขส.3 – บ้านหนองน้ำเกลี้ยง","สาย 5 : ตลาดหนองไผ่ล้อม – บ้านทุ่ม",
-            "สาย 6 : ตลาดเทศบาล 1 – บ้านเหล่านกชุม","สาย 8 : สามเหลี่ยม – บ้านดอนบม","สาย 10 : มหาวิทยาลัยขอนแก่น – สนามม้า","สาย 12 : พิพิธภัณฑ์ – รร.ท่าพระวิทยายน",
-            "สาย 18 : โรงเรียนกัลยาณวัตร – บ้านโนนพอง","สาย 19 : ตลาดหนองไผ่ล้อม – บ้านโนนเรือง","สาย 20 : ตลาดหนองไผ่ล้อม – บ้านกลางฮุง","สาย 22 : บ้านหนองไฮ- บ้านดอนหญ้านาง",
-            "สาย 23 : ตลาดเทศบาล 1 – บ้านห้วยเตย","หน้าเมือง","หลังหอกาฬ","เหล่านาดี"};
+    String[] nameArray = {
+            "สาย 2 : บ้านโคกฟันโปง – บ้านโคกน้อย",
+            "สาย 3 : บขส.3 – บ้านพรหมนิมิตร",
+            "สาย 4 : บขส.3 – บ้านหนองน้ำเกลี้ยง",
+            "สาย 5 : ตลาดหนองไผ่ล้อม – บ้านทุ่ม",
+            "สาย 6 : ตลาดเทศบาล 1 – บ้านเหล่านกชุม",
+            "สาย 8 : สามเหลี่ยม – บ้านดอนบม",
+            "สาย 9 : สามเหลี่ยม – บ้านสะอาด",
+            "สาย 10 : มหาวิทยาลัยขอนแก่น – สนามม้า",
+            "สาย 11 : บ้านโนนสวรรค์ – บ้านขามเจริญ",
+            "สาย 12 : พิพิธภัณฑ์ – รร.ท่าพระวิทยายน",
+            "สาย 13 : บ้านน้อยนิเวศน์ – บ้านสว่างมรรคา",
+            "สาย 16 : บ้านโนนทัน – วัดป่าอดุลยาราม",
+            "สาย 17 : บ้านโนนทัน – บ้านโคกท่า",
+            "สาย 18 : โรงเรียนกัลยาณวัตร – บ้านโนนพอง",
+            "สาย 19 : ตลาดหนองไผ่ล้อม – บ้านโนนเรือง",
+            "สาย 20 : ตลาดหนองไผ่ล้อม – บ้านกลางฮุง",
+            "สาย 21 : บ้านโนนเขวา – บ้านดอนยาง",
+            "สาย 22 : บ้านหนองไฮ- บ้านดอนหญ้านาง",
+            "สาย 23 : ตลาดเทศบาล 1 – บ้านห้วยเตย"};
 
     String[] infoArray = {
             "สาย 2 : บ้านโคกฟันโปง – บ้านโคกน้อย",
@@ -38,16 +56,19 @@ public class MapAllActivity extends FragmentActivity implements OnMapReadyCallba
             "สาย 5 : ตลาดหนองไผ่ล้อม – บ้านทุ่ม",
             "สาย 6 : ตลาดเทศบาล 1 – บ้านเหล่านกชุม",
             "สาย 8 : สามเหลี่ยม – บ้านดอนบม",
+            "สาย 9 : สามเหลี่ยม – บ้านสะอาด",
             "สาย 10 : มหาวิทยาลัยขอนแก่น – สนามม้า",
+            "สาย 11 : บ้านโนนสวรรค์ – บ้านขามเจริญ",
             "สาย 12 : พิพิธภัณฑ์ – รร.ท่าพระวิทยายน",
+            "สาย 13 : บ้านน้อยนิเวศน์ – บ้านสว่างมรรคา",
+            "สาย 16 : บ้านโนนทัน – วัดป่าอดุลยาราม",
+            "สาย 17 : บ้านโนนทัน – บ้านโคกท่า",
             "สาย 18 : โรงเรียนกัลยาณวัตร – บ้านโนนพอง",
             "สาย 19 : ตลาดหนองไผ่ล้อม – บ้านโนนเรือง",
             "สาย 20 : ตลาดหนองไผ่ล้อม – บ้านกลางฮุง",
+            "สาย 21 : บ้านโนนเขวา – บ้านดอนยาง",
             "สาย 22 : บ้านหนองไฮ- บ้านดอนหญ้านาง",
-            "สาย 23 : ตลาดเทศบาล 1 – บ้านห้วยเตย",
-            "หน้าเมือง",
-            "หลังหอกาฬ",
-            "เหล่านาดี"
+            "สาย 23 : ตลาดเทศบาล 1 – บ้านห้วยเตย"
     };
 
     Integer[] imageArray = {
@@ -57,16 +78,20 @@ public class MapAllActivity extends FragmentActivity implements OnMapReadyCallba
             R.drawable.ic_car5,
             R.drawable.ic_car6,
             R.drawable.ic_car8,
+            R.drawable.ic_car9,
             R.drawable.ic_car10,
+            R.drawable.ic_car11,
             R.drawable.ic_car12,
+            R.drawable.ic_car13,
+            R.drawable.ic_car16,
+            R.drawable.ic_car17,
             R.drawable.ic_car18,
             R.drawable.ic_car19,
             R.drawable.ic_car20,
+            R.drawable.ic_car21,
             R.drawable.ic_car22,
-            R.drawable.ic_car23,
-            R.drawable.logo,
-            R.drawable.logo,
-            R.drawable.logo};
+            R.drawable.ic_car23
+    };
 
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
@@ -134,23 +159,37 @@ public class MapAllActivity extends FragmentActivity implements OnMapReadyCallba
         mMap.addMarker(new MarkerOptions().position(latLng3).title("สาย 5 : ตลาดหนองไผ่ล้อม – บ้านทุ่ม").snippet("").icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_pin_marker)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng3));
 
-        LatLng latLng4 = new LatLng(16.425585, 102.834150);
-        mMap.addMarker(new MarkerOptions().position(latLng4).title("สาย 6 : ตลาดเทศบาล 1 – บ้านเหล่านกชุม").snippet("").icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_pin_marker)));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng4));
-
         LatLng latLng5 = new LatLng(16.389369, 102.806185);
         mMap.addMarker(new MarkerOptions().position(latLng5).title("สาย 8 : สามเหลี่ยม – บ้านดอนบม").snippet("").icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_pin_marker)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng5));
+
+        LatLng latLng14 = new LatLng(16.455835, 102.827421);
+        mMap.addMarker(new MarkerOptions().position(latLng14).title("สาย 9 : สามเหลี่ยม – บ้านสะอาด").snippet("").icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_pin_marker)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng14));
 
         LatLng latLng6 = new LatLng(16.389369, 102.806185);
         mMap.addMarker(new MarkerOptions().position(latLng6).title("สาย 10 : มหาวิทยาลัยขอนแก่น – สนามม้า").snippet("").icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_pin_marker)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng6));
 
-        //มลิวรรณ
+        LatLng latLng16 = new LatLng(16.409522, 102.781848);
+        mMap.addMarker(new MarkerOptions().position(latLng16).title("สาย 11 : บ้านโนนสวรรค์ – บ้านขามเจริญ").snippet("").icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_pin_marker)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng16));
 
         LatLng latLng8 = new LatLng(16.389369, 102.806185);
         mMap.addMarker(new MarkerOptions().position(latLng8).title("สาย 12 : พิพิธภัณฑ์ – รร.ท่าพระวิทยายน").snippet("").icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_pin_marker)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng8));
+
+        LatLng latLng15 = new LatLng(16.447973, 102.822856);
+        mMap.addMarker(new MarkerOptions().position(latLng15).title("สาย 13 : บ้านน้อยนิเวศน์ – บ้านสว่างมรรคา").snippet("").icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_pin_marker)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng15));
+
+        LatLng latLng17 = new LatLng(16.464537, 102.827373);
+        mMap.addMarker(new MarkerOptions().position(latLng17).title("สาย 16 : บ้านโนนทัน – วัดป่าอดุลยาราม").snippet("").icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_pin_marker)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng17));
+
+        LatLng latLng18 = new LatLng(16.389369, 102.806185);
+        mMap.addMarker(new MarkerOptions().position(latLng18).title("สาย 17 : บ้านโนนทัน – บ้านโคกท่า").snippet("").icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_pin_marker)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng18));
 
         LatLng latLng9 = new LatLng(16.426794, 102.874816);
         mMap.addMarker(new MarkerOptions().position(latLng9).title("สาย 18 : โรงเรียนกัลยาณวัตร – บ้านโนนพอง").snippet("").icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_pin_marker)));
@@ -164,17 +203,21 @@ public class MapAllActivity extends FragmentActivity implements OnMapReadyCallba
         mMap.addMarker(new MarkerOptions().position(latLng11).title("สาย 20 : ตลาดหนองไผ่ล้อม – บ้านกลางฮุง").snippet("").icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_pin_marker)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng11));
 
+        LatLng latLng19 = new LatLng(16.455550, 102.855093);
+        mMap.addMarker(new MarkerOptions().position(latLng19).title("สาย 21 : บ้านโนนเขวา – บ้านดอนยาง").snippet("").icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_pin_marker)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng19));
+
         LatLng latLng12 = new LatLng(16.414864, 102.874371);
         mMap.addMarker(new MarkerOptions().position(latLng12).title("สาย 22 : บ้านหนองไฮ- บ้านดอนหญ้านาง").snippet("").icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_pin_marker)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng12));
 
         LatLng latLng13 = new LatLng(16.438631, 102.834744);
         mMap.addMarker(new MarkerOptions().position(latLng13).title("สาย 23 : ตลาดเทศบาล 1 – บ้านห้วยเตย").snippet("").icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_pin_marker)));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng13,14));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng13));
 
-
-
-
+        LatLng latLng4 = new LatLng(16.425585, 102.834150);
+        mMap.addMarker(new MarkerOptions().position(latLng4).title("สาย 6 : ตลาดเทศบาล 1 – บ้านเหล่านกชุม").snippet("").icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_pin_marker)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng4, 14));
 
         mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
             @Override
@@ -230,7 +273,7 @@ public class MapAllActivity extends FragmentActivity implements OnMapReadyCallba
                     intent.putExtra("minibus", message);
                     String message2 = infoArray[4];
                     intent.putExtra("btsMinibus2", message2);
-                    Integer message3 = imageArray[5];
+                    Integer message3 = imageArray[4];
                     intent.putExtra("image", message3);
                     startActivity(intent);
                     overridePendingTransition(R.anim.push_in, R.anim.push_in_exit);
@@ -246,7 +289,7 @@ public class MapAllActivity extends FragmentActivity implements OnMapReadyCallba
                     startActivity(intent);
                     overridePendingTransition(R.anim.push_in, R.anim.push_in_exit);
 
-                }else if (marker.getTitle().equals("สาย 10 : มหาวิทยาลัยขอนแก่น – สนามม้า")) {
+                }else if (marker.getTitle().equals("สาย 9 : สามเหลี่ยม – บ้านสะอาด")) {
                     Intent intent = new Intent(MapAllActivity.this, MapsDetailActivity.class);
                     String message = nameArray[6];
                     intent.putExtra("minibus", message);
@@ -256,8 +299,7 @@ public class MapAllActivity extends FragmentActivity implements OnMapReadyCallba
                     intent.putExtra("image", message3);
                     startActivity(intent);
                     overridePendingTransition(R.anim.push_in, R.anim.push_in_exit);
-
-                }else if (marker.getTitle().equals("สาย 12 : พิพิธภัณฑ์ – รร.ท่าพระวิทยายน")) {
+                }else if (marker.getTitle().equals("สาย 10 : มหาวิทยาลัยขอนแก่น – สนามม้า")) {
                     Intent intent = new Intent(MapAllActivity.this, MapsDetailActivity.class);
                     String message = nameArray[7];
                     intent.putExtra("minibus", message);
@@ -268,7 +310,7 @@ public class MapAllActivity extends FragmentActivity implements OnMapReadyCallba
                     startActivity(intent);
                     overridePendingTransition(R.anim.push_in, R.anim.push_in_exit);
 
-                }else if (marker.getTitle().equals("สาย 18 : โรงเรียนกัลยาณวัตร – บ้านโนนพอง")) {
+                }else if (marker.getTitle().equals("สาย 11 : บ้านโนนสวรรค์ – บ้านขามเจริญ")) {
                     Intent intent = new Intent(MapAllActivity.this, MapsDetailActivity.class);
                     String message = nameArray[8];
                     intent.putExtra("minibus", message);
@@ -279,7 +321,8 @@ public class MapAllActivity extends FragmentActivity implements OnMapReadyCallba
                     startActivity(intent);
                     overridePendingTransition(R.anim.push_in, R.anim.push_in_exit);
 
-                }else if (marker.getTitle().equals("สาย 19 : ตลาดหนองไผ่ล้อม – บ้านโนนเรือง")) {
+                }
+                else if (marker.getTitle().equals("สาย 12 : พิพิธภัณฑ์ – รร.ท่าพระวิทยายน")) {
                     Intent intent = new Intent(MapAllActivity.this, MapsDetailActivity.class);
                     String message = nameArray[9];
                     intent.putExtra("minibus", message);
@@ -290,7 +333,7 @@ public class MapAllActivity extends FragmentActivity implements OnMapReadyCallba
                     startActivity(intent);
                     overridePendingTransition(R.anim.push_in, R.anim.push_in_exit);
 
-                }else if (marker.getTitle().equals("สาย 20 : ตลาดหนองไผ่ล้อม – บ้านกลางฮุง")) {
+                }else if (marker.getTitle().equals("สาย 13 : บ้านน้อยนิเวศน์ – บ้านสว่างมรรคา")) {
                     Intent intent = new Intent(MapAllActivity.this, MapsDetailActivity.class);
                     String message = nameArray[10];
                     intent.putExtra("minibus", message);
@@ -301,7 +344,7 @@ public class MapAllActivity extends FragmentActivity implements OnMapReadyCallba
                     startActivity(intent);
                     overridePendingTransition(R.anim.push_in, R.anim.push_in_exit);
 
-                }else if (marker.getTitle().equals("สาย 22 : บ้านหนองไฮ- บ้านดอนหญ้านาง")) {
+                }else if (marker.getTitle().equals("สาย 16 : บ้านโนนทัน – วัดป่าอดุลยาราม")) {
                     Intent intent = new Intent(MapAllActivity.this, MapsDetailActivity.class);
                     String message = nameArray[11];
                     intent.putExtra("minibus", message);
@@ -312,7 +355,7 @@ public class MapAllActivity extends FragmentActivity implements OnMapReadyCallba
                     startActivity(intent);
                     overridePendingTransition(R.anim.push_in, R.anim.push_in_exit);
 
-                }else if (marker.getTitle().equals("สาย 23 : ตลาดเทศบาล 1 – บ้านห้วยเตย")) {
+                }else if (marker.getTitle().equals("สาย 17 : บ้านโนนทัน – บ้านโคกท่า")) {
                     Intent intent = new Intent(MapAllActivity.this, MapsDetailActivity.class);
                     String message = nameArray[12];
                     intent.putExtra("minibus", message);
@@ -323,7 +366,7 @@ public class MapAllActivity extends FragmentActivity implements OnMapReadyCallba
                     startActivity(intent);
                     overridePendingTransition(R.anim.push_in, R.anim.push_in_exit);
 
-                }else if (marker.getTitle().equals("หลังหอกาฬ")) {
+                }else if (marker.getTitle().equals("สาย 18 : โรงเรียนกัลยาณวัตร – บ้านโนนพอง")) {
                     Intent intent = new Intent(MapAllActivity.this, MapsDetailActivity.class);
                     String message = nameArray[13];
                     intent.putExtra("minibus", message);
@@ -334,7 +377,7 @@ public class MapAllActivity extends FragmentActivity implements OnMapReadyCallba
                     startActivity(intent);
                     overridePendingTransition(R.anim.push_in, R.anim.push_in_exit);
 
-                }else if (marker.getTitle().equals("เหล่านาดี")) {
+                }else if (marker.getTitle().equals("สาย 19 : ตลาดหนองไผ่ล้อม – บ้านโนนเรือง")) {
                     Intent intent = new Intent(MapAllActivity.this, MapsDetailActivity.class);
                     String message = nameArray[14];
                     intent.putExtra("minibus", message);
@@ -345,7 +388,7 @@ public class MapAllActivity extends FragmentActivity implements OnMapReadyCallba
                     startActivity(intent);
                     overridePendingTransition(R.anim.push_in, R.anim.push_in_exit);
 
-                }else if (marker.getTitle().equals("มลิวรรณ")) {
+                }else if (marker.getTitle().equals("สาย 20 : ตลาดหนองไผ่ล้อม – บ้านกลางฮุง")) {
                     Intent intent = new Intent(MapAllActivity.this, MapsDetailActivity.class);
                     String message = nameArray[15];
                     intent.putExtra("minibus", message);
@@ -356,10 +399,53 @@ public class MapAllActivity extends FragmentActivity implements OnMapReadyCallba
                     startActivity(intent);
                     overridePendingTransition(R.anim.push_in, R.anim.push_in_exit);
 
+                }else if (marker.getTitle().equals("สาย 21 : บ้านโนนเขวา – บ้านดอนยาง")) {
+                    Intent intent = new Intent(MapAllActivity.this, MapsDetailActivity.class);
+                    String message = nameArray[16];
+                    intent.putExtra("minibus", message);
+                    String message2 = infoArray[16];
+                    intent.putExtra("btsMinibus2", message2);
+                    Integer message3 = imageArray[16];
+                    intent.putExtra("image", message3);
+                    startActivity(intent);
+                    overridePendingTransition(R.anim.push_in, R.anim.push_in_exit);
+
+                }else if (marker.getTitle().equals("สาย 22 : บ้านหนองไฮ- บ้านดอนหญ้านาง")) {
+                    Intent intent = new Intent(MapAllActivity.this, MapsDetailActivity.class);
+                    String message = nameArray[17];
+                    intent.putExtra("minibus", message);
+                    String message2 = infoArray[17];
+                    intent.putExtra("btsMinibus2", message2);
+                    Integer message3 = imageArray[17];
+                    intent.putExtra("image", message3);
+                    startActivity(intent);
+                    overridePendingTransition(R.anim.push_in, R.anim.push_in_exit);
+
+                }else if (marker.getTitle().equals("สาย 23 : ตลาดเทศบาล 1 – บ้านห้วยเตย")) {
+                    Intent intent = new Intent(MapAllActivity.this, MapsDetailActivity.class);
+                    String message = nameArray[18];
+                    intent.putExtra("minibus", message);
+                    String message2 = infoArray[18];
+                    intent.putExtra("btsMinibus2", message2);
+                    Integer message3 = imageArray[18];
+                    intent.putExtra("image", message3);
+                    startActivity(intent);
+                    overridePendingTransition(R.anim.push_in, R.anim.push_in_exit);
+
                 }
             }
         });
 
+        Polygon polygon = googleMap.addPolygon(new PolygonOptions()
+                .clickable(true)
+                .add(
+                        new LatLng(16.519279, 102.797877),
+                        new LatLng(16.510163, 102.917247),
+                        new LatLng(16.365698, 102.880722),
+                        new LatLng(16.384732, 102.761325)
+                ));
+        polygon.setTag("alpha");
+        polygon.setStrokeColor(getResources().getColor(R.color.colorAccent));
     }
 
 
